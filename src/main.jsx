06fa2ba4d27214346b2c98085b9add1dd3391210
Promit-08 <joyacher@gmail.com>
2026-04-21 00:40:58 +0600

    banckend 1

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App"; // ✅ default export
+import App from "./App.jsx"; // ✅ default export
 import "./index.css"; // global styles
 
 ReactDOM.createRoot(document.getElementById("root")).render(
